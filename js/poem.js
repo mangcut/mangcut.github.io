782c3a7396cba9e1915de15ace5d509763566417
Truong Hong Thi <thi1981@gmail.com>
2013-04-20 15:33:16 +0700

    Change to use Poem framework

---
+++ js/poem.js
@@ -0,0 +1,321 @@
+$(document).ready(function () {
+
+	// on PC, FireFox does not support yet, Safari support a little
+	if (window.Modernizr) {
+		if (!Modernizr.inputtypes.date || !Modernizr.touch) {
+			var $date = $("input[type='date']");
+			if ($date.datepicker) {
+				if ($date.data("date-format")) {
+					$date.prop("type", "text").datepicker();
+				} else {
+					$date.prop("type", "text").datepicker({format: 'dd/mm/yyyy'});
+				}
+			}
+		}
+	}
+    var today = new Date();
+    var year = today.getFullYear().toString();
+    var month = (today.getMonth() + 1).toString();
+    if (month.length < 2) month = "0" + month;
+    var day = today.getDate().toString();
+    if (day.length < 2) day = "0" + day;
+	var dateText = day + "/" + month + "/" + year;
+    $("[data-value='today']").val(dateText);
+	$("[data-text='today']").text(dateText);
+	$("[data-prompt='today']").attr("placeholder", dateText);
+	
+	// prevent page-load on <a href='#'>Something</a>
+	$("a").live("click", function(e) {
+		if ($(this).attr("href") === "#") {
+			e.preventDefault();
+		}
+	});
+
+	// auto switch
+	$(".switch").flipswitch();
+	
+	// auto action
+	$("[data-action]").perform();
+	
+});
+
+(function( $ ){
+    var actionDefaults = {
+		action: "toggle",
+		timing: "click"
+		//target: "selector",
+		//relative: "closest",
+		//options: [array of arguments],
+		//advanced: some extra settings
+	};
+	
+	var doAction = function($target, args) {
+		//if (args.action === ":xxx") { // special support for special action, should start with :
+			//
+		//} else { // trigger, toggle, show, hide, effect, toggleClass, addClass, removeClass, switchClass, remove, detach, etc.
+			doCoreAction($target, args);
+		//}
+	};
+	
+	var doCoreAction = function($target, args) {
+		if (args.action === "toggle" || args.action === "show") {
+			if (args.advanced === "overlay") {
+				var $overlay = $('#overlay');
+				if ($overlay.length === 0) {
+					$("<div id='overlay'></div>").appendTo($("body")).show().click(function() {
+						$target.hide(args.options);
+						$(this).fadeOut();
+					});
+				} else {
+					$overlay.show();
+				}
+			}
+		}
+		
+		if (args.action === "remove" || args.action === "detach") {
+			if (typeof args.options === "object") {
+				$target.hide($.extend({}, args.options, {
+					complete: function() {
+						doGenericAction($target, args);
+					}}));
+			} else {
+				$target.hide(args.options, function() {
+						doGenericAction($target, args);
+					});
+			}
+		} else {
+			doGenericAction($target, args);
+		}
+	};
+	
+	var doGenericAction = function($target, args) {
+		var actionName = args.action.replace(/\(\)$/, "");
+		var parts = actionName.split(".");
+		if (!args.target) {
+			if (parts.length === 1) {
+				$.each([$target, window, $(args.self), args.self], function(index, value){
+					$target = value;
+					return !$target[actionName];
+				});
+			}
+			
+			if (parts[0] === "window") {
+				$target = window;
+				parts = parts.slice(1);
+			} else if (parts[0] === "this") {
+				$target = args.self;
+				parts = parts.slice(1);
+			} else if (parts[0] === "$(this)") {
+				$target = $(args.self);
+				parts = parts.slice(1);
+			}
+		}
+				
+		var len = parts.length;	
+		if (len > 0) {
+			actionName = parts[len - 1];
+		}
+		if (len > 1) {
+			parts = parts.slice(0, len - 1);
+			$target = walk($target, parts);
+		}
+		if (typeof $target[actionName] === "function") {
+			if ($.isArray(args.options)) {
+				$target[actionName].apply(this, args.options);
+			} else {
+				$target[actionName](args.options);
+			}
+		} else {
+			$target[actionName] = args.options;
+		}
+	};
+	
+	var performAction = function(actionInfo, selfTag) {
+		var actionArray;
+		if ($.isArray(actionInfo)) {
+			actionArray = actionInfo;
+		} else {
+			actionArray = [actionInfo];
+		}
+		
+		$.each(actionArray, function(index, value) {
+			var args = $.extend({}, actionDefaults, ((typeof value === "object") ? value : {action: value}));
+			args.self = selfTag;
+
+			// special support for some action
+			if (args.action === "tooltip" && !value.timing) {
+				args.timing = "now";
+			}
+			
+			var internalPerform = function() {
+				if ($P.nullOrEmpty(args.condition) || $(args.condition).length) {
+					var $target = null;
+					if (args.target) {
+						if (typeof args.target === "object") {
+							$target = args.target;
+						} else {
+							if (args.relative) {
+								$target = $(selfTag)[args.relative](args.target);
+							} else {
+								$target = $(args.target);
+							}
+						}
+					} else {
+						$target = window;
+					}
+					doAction($target, args);
+				}
+			};
+			
+			if (args.timing === "now") {
+				internalPerform();
+			} else {
+				$(selfTag).live(args.timing, internalPerform);
+			}
+		});
+		};
+	
+	var walk = function(target, steps) {
+		$.each(steps, function(index, step) {
+			step.replace(/\(\)$/, "");
+			target = target[step];
+			if (typeof target === "function") {
+				target = target();
+			}
+		});
+		
+		return target;
+	};
+	
+	$.fn.perform = function() {
+		return this.each(function() {
+			var $this = $(this);
+			var action = $this.data("action");
+			if (action) {
+				if (typeof action === "object") {
+					performAction(action, this);
+				} else {
+					performAction($this.data(), this);
+				}
+			}
+		});
+  }	
+})( jQuery );
+
+(function( $ ){
+	$.fn.flipswitch = function() {
+		var template = 
+			'<label class="switch-ui">' +
+			'	<div class="switch-inner"></div>' +
+			'	<div class="switch-switch"></div>' +
+			'</label>';
+		var $switchUI;
+		return this.hide().wrap("<div class='switch-wrapper' />").after(function() {
+			if ($switchUI) {
+				$switchUI = $switchUI.clone();
+			} else {
+				$switchUI = $(template);
+			}
+			return $switchUI.attr("for", this.id);
+		});
+	}
+})( jQuery );
+
+(function( global ) {
+  var Poem = (function() {
+
+    return {
+		checkAll: function(target, notHilight) {
+			var $target = $(target);
+			if ($target.not(":checked").length) {
+				$target.prop('checked', true);
+				$(this).prop('checked', true);
+			} else {
+				$target.prop('checked', false);
+				$(this).prop('checked', false);
+			}
+			
+			if (!notHilight) {
+				$target.each(function() {
+					$c = $(this);
+					if ($c.closest("table.table-select")) {
+						var checked = $c.prop('checked');
+						if (checked) {
+							$c.closest("tr").addClass("selected");
+						} else {
+							$c.closest("tr").removeClass("selected");
+						}
+					}
+				});
+			}
+		},
+		// getData with default. Please note the passed-in object must be a jQuery object
+		getData: function(jObject, dataName, defaultValue) {
+				var value = jObject.data(dataName);
+				if (this.nullOrEmpty(value)) {
+					value = defaultValue;
+				}
+				
+				return value;
+		},
+		format: function() {
+		  var s = arguments[0];
+		  for (var i = 0; i < arguments.length - 1; i++) {
+			var reg = new RegExp("\\{" + i + "\\}", "gm");
+			s = s.replace(reg, arguments[i + 1]);
+		  }
+
+		  return s;
+		},
+		param: function(name) {
+			var match = RegExp('[?&]' + name + '=([^&]*)')
+							.exec(window.location.search);
+			return match && decodeURIComponent(match[1].replace(/\+/g, ' '));
+		},
+		addCommas: function(nStr) {
+			nStr += '';
+			x = nStr.split('.');
+			x1 = x[0];
+			x2 = x.length > 1 ? '.' + x[1] : '';
+			var rgx = /(\d+)(\d{3})/;
+			while (rgx.test(x1)) {
+				x1 = x1.replace(rgx, '$1' + ',' + '$2');
+			}
+			return x1 + x2;
+		},
+		padZeros: function(number, length) {
+			var str = '' + number;
+			while (str.length < length) {
+				str = '0' + str;
+			}
+			return str;
+		},
+		nullOrEmpty: function(something) {
+			if (typeof something === 'undefined' || something === null || something === "") {
+				return true;
+			}
+			return false;
+		},
+		toggleColumn: function(table, index, effect) {
+			return $(table).find('td:nth-child(' + index + '),th:nth-child(' + index + ')').toggle(effect || "fade");
+		},
+		alert: function(message, title) {
+			var appTitle = title || this.AppTitle || document.title;
+			alert(message);
+		},
+		ask: function(message, title) {
+			var appTitle = title || this.AppTitle || document.title;
+			confirm(message);
+		},
+		confirm: function(message, title) {
+			var appTitle = title || this.AppTitle || document.title;
+			confirm(message);
+		}
+    };
+  })();
+
+  // expose our module to the global object
+  global.$P = Poem;
+
+})( this );
+
