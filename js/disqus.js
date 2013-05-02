ad20585ce74be6164fe40f7ab50eabc903289d62
Truong Hong Thi <thi1981@gmail.com>
2013-05-02 14:59:25 +0700

    Fix some IE issues

--- js/disqus.js
+++ js/disqus.js
@@ -10,7 +10,7 @@ if (disqus_identifier) {
 /* * * DON'T EDIT BELOW THIS LINE * * */
 (function() {
 	var dsq = document.createElement('script'); dsq.type = 'text/javascript'; dsq.async = true;
-	dsq.src = 'http://' + disqus_shortname + '.disqus.com/embed.js';
+	dsq.src = '//' + disqus_shortname + '.disqus.com/embed.js';
 	(document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(dsq);
 })();
 
