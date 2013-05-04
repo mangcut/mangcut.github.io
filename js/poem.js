df5626c09f054eb177e4de4bb46f4c9bd71a8bd3
Truong Hong Thi <thi1981@gmail.com>
2013-05-04 23:09:16 +0700

    Add Facebook like

--- js/poem.js
+++ js/poem.js
@@ -8,6 +8,17 @@
   ga('send', 'pageview');
 /* End Goolge Analytics */
 
+/* Facebook Like */
+$("#fbLikePoem").data("href", location.href);
+(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/en_US/all.js#xfbml=1";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
+/* End Facebook Like */
+
 $(document).ready(function () {
 	// prevent page-load on <a href='#'>Something</a>
 	$(document).on("click", "a", function(e) {
