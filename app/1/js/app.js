2a18c8716911475007126e526bb5ed236e8989c1
Truong Hong Thi <thi1981@gmail.com>
2013-07-28 19:06:51 +0700

    Calculate size

--- app/1/js/app.js
+++ app/1/js/app.js
@@ -12,8 +12,12 @@ var cells = [0, 0, 1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7];
 
 var startTime;
 var lonelyCell;
+var secondCounter;
 
 $(document).ready(function () {
+	// build the play ground
+	buildPlayGround(4, 4);
+	
 	// setup menu
 	$("#menu td").tap(function() {
 		if (!$(this).hasClass("disabled")) {
@@ -50,6 +54,8 @@ $(document).ready(function () {
 					// if win
 					if ($("#playGround .row div.fixed").length === 16) {
 						$("body").css({'background-color': 'pink'});
+						clearInterval(secondCounter);
+						secondCounter = null;
 					}
 				} else {
 					// not match -> close both cell
@@ -76,7 +82,9 @@ function setCellColor($item, color) {
 
 function start() {
 	lonelyCell = null;
-	startTime = new Date();
+	clearInterval(secondCounter);
+	secondCounter = null;
+	
 	cells = shuffle(cells);
 	setCellColor($("#playGround .row div"), 'grey').each(function(index) {
 		$(this).data("color", colors[cells[index]]).removeClass("open fixed");
@@ -84,6 +92,11 @@ function start() {
 	
 	$("#counter").text("000");
 	$("body").css({'background-color': '#f0f0f0'});
+	
+	startTime = new Date();
+	secondCounter = window.setInterval(function() {
+		$("#counter").text(secondPassed());
+	}, 1000)
 }
 
 function toggleMenu() {
@@ -102,7 +115,7 @@ function shuffle(array) {
     // While there are elements in the array
     while (counter > 0) {
         // Pick a random index
-        index = (Math.random() * counter--) | 0;
+        index = (Math.random() * counter--) | 0; // bitwise for Math.floor
 
         // And swap the last element with it
         temp = array[counter];
@@ -111,4 +124,41 @@ function shuffle(array) {
     }
 
     return array;
+}
+
+function secondPassed() {
+	var endTime = new Date();
+	var passedSeconds = ((endTime - startTime) / 1000) | 0; // bitwise for Math.floor
+	if (passedSeconds > 999) passedSeconds = 999;
+	if (passedSeconds < 10) {
+		return "00" + passedSeconds;
+	} else if (passedSeconds < 100) {
+		return "0" + passedSeconds;
+	}
+}
+
+function buildPlayGround(col, row) {
+	var width = calcCellSize(4, 4);
+	var sw = "width:" + width + "px";
+	var sh = "height:" + width + "px";
+	var style = "style='" + sw + ";" + sh + ";'";
+	var html = "";
+	for (var i = 0; i < row; i++)  {
+		var rowHtml = "<div class='row'>";
+		for (var j = 0; j < col; j++) {
+			rowHtml += "<div " + style + "></div>";
+		}
+		rowHtml += "</div>";
+		html += rowHtml;
+	}
+	
+	$("#playGround").html(html);
+}
+
+// assume cell must be square
+function calcCellSize(col, row) {
+	var w = window.innerWidth;
+	var x = (w / col) | 0;
+	
+	return x;
 }
