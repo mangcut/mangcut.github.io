8710e9840a5e619ebedd285929f17a48837e94e8
Truong Hong Thi <thi1981@gmail.com>
2016-03-24 20:53:01 +0700

    up

---
+++ _includes/customJS/linkhay_bot.js
@@ -0,0 +1,5 @@
+var _vietIdApp_Key = 'bc3e81904b168ccba739321916e96450';
+var lc = document.createElement('script'); lc.type = "text/javascript";
+lc.src = "http://mingid.vcmedia.vn/js/vietid_comment.js?t=2";
+var s = document.getElementsByTagName('script')[0];
+s.parentNode.insertBefore(lc, s);
