b48c7de0b22a52fe8d117f52cb59fe4a026dd694
Truong Hong Thi <thi1981@gmail.com>
2017-10-07 04:24:41 +0700

    up

--- kb/js/my-app.js
+++ kb/js/my-app.js
@@ -20,10 +20,100 @@ var storePicker = myApp.picker({
     ]
 });
 
+// playground requires you to assign document definition to a variable called dd
+
+var pdfDef = {
+	content: [
+		{ 
+			text: 'PHIẾU XUẤT KHO\n\n', 
+			style: 'header' 
+		},
+		{
+			text: [
+				{ text: 'Tên kho: ', bold: true },
+				'Nghi Sơn\n',
+				{ text: 'Tên quản lý kho: ', bold: true },
+				'Nguyễn Hải Hà\n',
+				{ text: 'Thời gian: ', bold: true },
+				'18:45, ngày 27/4/2018\n\n',
+				{ text: 'Biển số xe: ', bold: true },
+				'36A-3451\n',
+				{ text: 'Tài xế: ', bold: true },
+				'Lê Văn Việt\n',
+				{ text: 'Kích thước thùng xe: ', bold: true },
+				'5',
+				{ text: ' (m3)\n\n', fontSize: 10},
+				{ text: 'Cung đường: ', bold: true },
+				'Tĩnh Gia - Ngọc Lặc\n\n',
+				{ text: 'Loại hàng hóa: ', bold: true },
+				'Xi măng\n',
+				{ text: 'Số lượng: ', bold: true },
+				'35\n\n',
+			]
+		},
+		{ text: 'Hình ảnh kèm theo:', bold: true }
+	],
+	styles: {
+		header: {
+			fontSize: 15,
+			bold: true
+		}
+	}
+}
+
+function getDataUri(img) {
+	var canvas = document.createElement('canvas');
+	canvas.width = 120 * img.naturalWidth / img.naturalHeight;
+	canvas.height = 120;
+
+	canvas.getContext('2d').drawImage(img, 0, 0, canvas.width, canvas.height);
+
+	return {
+		image: canvas.toDataURL('image/png'),
+		width: canvas.width,
+		height: canvas.height
+	}
+}
+
 // Callbacks to run specific code for specific pages, for example for About page:
-myApp.onPageInit('about', function (page) {
-    // run createContentPage func after link was clicked
-    $$('.create-page').on('click', function () {
-        createContentPage();
-    });
+myApp.onPageInit('form', function (page) {
+	FileReaderJS.setupInput(document.getElementById("fileImg"), {
+	readAsDefault: "DataURL",
+	on: {
+		load: function(e, file) {
+			var img = new Image();
+			img.onload = function() {
+				$$("<div class='small-img'/>")
+					.append(img)
+					.append('<i class="f7-icons">close</i>')
+					.appendTo(".small-img-list");
+			};
+			img.src = e.target.result;
+		}
+	}
+	});
+	
+	$$('#btnAddImg').on('click', function () {
+		document.getElementById("fileImg").click();
+	});
+	$$('.small-img i').on('click', function () {
+		$$(this).parent().remove();
+	});
+	$$('.voucher-type').on('click', function () {
+		$$('.voucher-type').toggleClass("active");
+	});
+	$$('#btnCreate').on('click', function () {
+		$$('.small-img img').each(function(){
+			var uri = getDataUri(this);
+			console.log(uri);
+			pdfDef.content.push(uri);
+		});
+		pdfDef.content.push({
+			text: [
+				{ text: '\nGhi chú: ', bold: true },
+				'N/A\n\n',
+			]
+		});
+		pdfMake.createPdf(pdfDef).open();
+	});	
 });
